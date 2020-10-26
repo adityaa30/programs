02c104b2eb1c2ea7302768e20acda11b53f57495
Aditya Kumar <k.aditya00@gmail.com>
2020-10-26 20:34:18 +0530

    feat(strings): Prefix Function

---
+++ Strings/CountOccurencesOfPrefix.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> PrefixFunction(string &s) {
+  int n = (int) s.length();
+  vector<int> lps(n, 0);
+  for (int i = 1; i < n; ++i) {
+    int j = lps[i - 1];
+    while (j > 0 && s[i] != s[j]) j = lps[j - 1];
+    j += (int) s[i] == s[j];
+    lps[i] = j;
+  }
+  return lps;
+}
+
+vector<int> PrefixOccurences(string &s) {
+  int n = (int) s.length();
+  auto lps = PrefixFunction(s);
+  vector<int> count(n + 1);
+  // count how many times it occurs in the array lps
+  for (int i = 0; i < n; ++i) count[lps[i]]++;
+
+  // we know that the length prefix i appears exactly ans[i] times, 
+  // then this number must be added to the number of occurrences 
+  // of its longest suffix that is also a prefix
+  for (int i = n - 1; i > 0; --i) count[lps[i - 1]] += count[i];
+  
+  // add 1 to count the original prefixes
+  for (int i = 0; i <= n; ++i) count[i]++;
+  
+  return count;
+}
+
+int32_t main() {
+  string str = "aaaa";
+  auto count = PrefixOccurences(str);
+  for (int i = 0; i < str.length(); ++i) {
+    cout << str.substr(0, i + 1) << ' ' << count[i + 1] << '\n';
+  }
+  return 0;
+}
---
+++ Strings/PrefixFunction.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> PrefixFunction(string s) {
+  int n = (int)s.length();
+  // largest prefix which matches with suffix ending at i
+  vector<int> lps(n);
+  /*
+  Observation 1:
+  lps[i] always increases by 1,
+  remains the same or decreases by any positive value
+
+  => lps[i + 1] > (lps[i] + 1) 
+  => `lps[i] = lps[i + 1] - 1 (removing the last character)
+  => `lps[i] now becomes better than lps[i]
+  => Contradiction
+
+  => Hence the prefix function can always increase by n for the
+  entire string. 
+  => Therefore, we just have to perform at max n string operations.
+
+  Observation 2:
+  if (s[i + 1] == s[lps[i]]) => lps[i + 1] = lps[i] + 1
+  else
+  => We need to try a shorter string
+  => Let's move immediately to j < lps[i] s.t. s[0 .. j - 1] = s[i - j + 1 .. i]
+
+  */
+
+  for (int i = 1; i < n; i++) {
+    int j = lps[i - 1];
+    while (j > 0 && s[i] != s[j]) j = lps[j - 1];
+    if (s[i] == s[j]) j++;
+    lps[i] = j;
+  }
+  return lps;
+}
+
+int32_t main() {
+  string str = "abcdabcd";
+  auto lps = PrefixFunction(str);
+  for (int i = 0; i < str.length(); ++i) {
+    cout << str[i] << ' ' << lps[i] << '\n';
+  }
+  return 0;
+}
