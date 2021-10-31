8c96cf79b315f9afe3d1f1f00535d0b15b461df6
Deepa Yadav <46419774+deepaa28@users.noreply.github.com>
2021-10-31 21:34:35 +0530

    chore(cses-dp): update Money Sums

--- ContestFiles/CSES/Dynamming Programming/Money-Sums.cpp
+++ ContestFiles/CSES/Dynamming Programming/Money-Sums.cpp
@@ -1,20 +1,29 @@
 #include <bits/stdc++.h>
+#define int long long int
 using namespace std;
 
-const int mxS = 100 * 1000;
+const int mxS = 100 * 1000 + 5;
 
 int32_t main() {
   int n;
-  scanf("%d", &n);
-  int vals[n];
-  for (int i = 0; i < n; ++i) scanf("%d", &vals[i]);
+  cin >> n;
+
   bitset<mxS> dp;
   dp[0] = 1;
-  for (int i = 0; i < n; ++i) dp = dp | (dp << vals[i]);
 
-  printf("%d\n", dp.count() - 1);
-  for (int i = 1; i < mxS; ++i)
-    if (dp[i]) printf("%d ", i);
+  for (int i = 0; i < n; ++i) {
+    int x;
+    cin >> x;
+    dp = dp | (dp << x);
+  }
+
+  cout << (dp.count() - 1) << '\n';
+  for (int s = 1; s < mxS; ++s) {
+    if (dp[s]) {
+      cout << s << ' ';
+    }
+  }
+  cout << '\n';
 
   return 0;
 }
