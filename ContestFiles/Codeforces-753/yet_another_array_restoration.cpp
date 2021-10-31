9e18b1f2fe9c110ac4fa141d76652f6fa6cb7d67
Karan Purswani <purskara@amazon.com>
2021-10-31 23:37:27 +0530

    Codeforces Round #753 Div3

---
+++ ContestFiles/Codeforces-753/yet_another_array_restoration.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve(){
+    int n,x,y;
+    cin>>n>>x>>y;
+    int j = n-1;
+    int d;
+    while(true){
+        if((y-x)%j == 0){
+            d = (y-x)/j;
+            break;
+        }
+        else{
+            j--;
+        };
+    }
+    int a=y;
+    int k = n-1;
+    while(k>0){
+        if(a-d>0){
+            k--;
+            a = a-d;
+        }
+        else{
+            break;
+        }
+    }
+    for(int i=0; i<n; i++){
+        cout<<a<<" ";
+        a=a+d;
+    }
+    cout<<endl;
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
